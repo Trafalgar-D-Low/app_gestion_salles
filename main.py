346d706aea03cfb7fdd94d257e0bf88dadf376fd
Anis Benabbas <benabbasanis50@gmail.com>
2026-04-18 22:47:28 -0400

    Lancement de l’interface graphique

--- main.py
+++ main.py
@@ -1,29 +1,4 @@
-from services.services_salle import ServiceSalle
-from models.salle import Salle
+from views.view_salle import ViewSalle
 
-service = ServiceSalle()
-
-# Ajouter une salle
-s1 = Salle("B205", "Labo INFO", "Laboratoire", 20)
-resultat, msg = service.ajouter_salle(s1)
-print(msg)
-
-# Lister toutes les salles
-print("\n------------Liste des salles------------")
-for salle in service.recuperer_salles():
-    salle.afficher_infos()
-
-# Modifier une salle
-s1.description = "Labo modifié"
-resultat, msg = service.modifier_salle(s1)
-print(msg)
-
-# Rechercher une salle
-print("\n------------Recherche B205------------")
-trouve = service.rechercher_salle("B205")
-if trouve:
-    trouve.afficher_infos()
-
-# Supprimer une salle
-service.supprimer_salle("B205")
-print("Salle supprimée!")
+app = ViewSalle()
+app.mainloop()
