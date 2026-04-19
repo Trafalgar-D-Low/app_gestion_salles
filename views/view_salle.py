355089d4db2f18a9d359e7e71d1db28bc344d0f9
Anis Benabbas <benabbasanis50@gmail.com>
2026-04-18 22:18:04 -0400

    créer la méthode modifier_salle

--- views/view_salle.py
+++ views/view_salle.py
@@ -37,7 +37,7 @@ class ViewSalle(ctk.CTk):
         self.btn_ajouter = ctk.CTkButton(self.cadreActions, text="Ajouter", command=self.ajouter_salle)
         self.btn_ajouter.grid(row=0, column=0, padx=10, pady=10)
 
-        self.btn_modifier = ctk.CTkButton(self.cadreActions, text="Modifier")
+        self.btn_modifier = ctk.CTkButton(self.cadreActions, text="Modifier", command=self.modifier_salle)
         self.btn_modifier.grid(row=0, column=1, padx=10, pady=10)
 
         self.btn_supprimer = ctk.CTkButton(self.cadreActions, text="Supprimer")
@@ -58,6 +58,22 @@ class ViewSalle(ctk.CTk):
             return
         salle = Salle(code, description, categorie, capacite)
         ok, msg = self.service_salle.ajouter_salle(salle)
+        messagebox.showinfo("Info", msg)
+        if ok:
+            self.lister_salles()
+
+    def modifier_salle(self):
+        code = self.entry_code.get()
+        description = self.entry_description.get()
+        categorie = self.entry_categorie.get()
+        capacite = self.entry_capacite.get()
+        try:
+            capacite = int(capacite)
+        except ValueError:
+             messagebox.showerror("Erreur", "La capacité doit être un nombre entier!")
+             return
+        salle = Salle(code, description, categorie, capacite)
+        ok, msg = self.service_salle.modifier_salle(salle)
         messagebox.showinfo("Info", msg)
         if ok:
             self.lister_salles()
