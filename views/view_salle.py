9fd7b66ad53d36766ab60ecfaf925df1b7c7d0d8
Anis Benabbas <benabbasanis50@gmail.com>
2026-04-18 21:46:50 -0400

    Implémentation du cadre Informations Salle

--- views/view_salle.py
+++ views/view_salle.py
@@ -9,3 +9,23 @@ class ViewSalle(ctk.CTk):
         self.title = "Gestion des salles"
         self.geometry = "600x600"
         self.service_salle = ServiceSalle()
+
+        # cadre Informations Salle
+        self.cadreInfo = ctk.CTkFrame(self, corner_radius=10)
+        self.cadreInfo.pack(pady=10, padx=10, fill="x")
+
+        ctk.CTkLabel(self.cadreInfo, text="Code :").grid(row=0, column=0, padx=10, pady=5, sticky="w")
+        self.entry_code = ctk.CTkEntry(self.cadreInfo)
+        self.entry_code.grid(row=0, column=1, padx=10, pady=5)
+
+        ctk.CTkLabel(self.cadreInfo, text="Description :").grid(row=1, column=0, padx=10, pady=5, sticky="w")
+        self.entry_description = ctk.CTkEntry(self.cadreInfo)
+        self.entry_description.grid(row=1, column=1, padx=10, pady=5)
+
+        ctk.CTkLabel(self.cadreInfo, text="Catégorie :").grid(row=2, column=0, padx=10, pady=5, sticky="w")
+        self.entry_categorie = ctk.CTkEntry(self.cadreInfo)
+        self.entry_categorie.grid(row=2, column=1, padx=10, pady=5)
+
+        ctk.CTkLabel(self.cadreInfo, text="Capacité :").grid(row=3, column=0, padx=10, pady=5, sticky="w")
+        self.entry_capacite = ctk.CTkEntry(self.cadreInfo)
+        self.entry_capacite.grid(row=3, column=1, padx=10, pady=5)
